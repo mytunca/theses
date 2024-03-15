fe03c3b6f313fefccb92ddc9cc4e3fce6de4dedb
Muhammet Yunus TUNCA <muhammedytunca@gmail.com>
2024-03-15 11:49:14 +0300

    Updates

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# theses
-YÖK Ulusal Tez Merkezi'nden veri kazıma aracı
+## YÖK Ulusal Tez Merkezi Veri Kazıma Aracı
+
+Bu "repository", Yükseköğretim Kurulu (YÖK) Ulusal Tez Merkezi'nde bulunan tezlerin detaylı verilerine erişimi kolaylaştırmak için geliştirilen bir JavaScript kodunu içerir.
+
+YÖK Ulusal Tez Merkezi'nde, tezlerle ilgili temel bilgiler (tez no, yazar, yıl, tez adı, üniversite, dil, tez türü ve konu) bir tablo halinde listelenir. Ancak, daha ayrıntılı bilgilere erişmek için her bir tezin detay sayfasına tek tek tıklamak gereklidir. Bu süreci otomatikleştirmek için geliştirilen bu JavaScript kodu, web tarayıcısı geliştirici araçları (DevTools) kullanılarak tablodaki tüm tezlerin detaylı verilerine erişimi sağlar.
+
+**Özellikler:**
+
+- YÖK Ulusal Tez Merkezi'nde listelenen tezlerin detaylı verilerine erişimi otomatikleştirir.
+- Tez numarası, yazar adı, yayın yılı, tez adı, tez türü ve konu gibi bilgilerin yanı sıra daha fazla detay içeren verilere ((dizin, sayfa sayısı, özet)) erişim sağlar.
+- JavaScript kullanarak tarayıcıda çalışır, ayrı bir yazılım yüklemenizi gerektirmez.
+- Verileri Microsoft Excel dosyası olarak indirmenize olanak tanır.
+
+Bu repo, araştırmacılar ve veri bilimcileri için YÖK Ulusal Tez Merkezi'nden veri toplamayı ve analiz etmeyi kolaylaştırmayı amaçlamaktadır.
+
+**Kullanım:**
+
+1. YÖK Ulusal Tez Merkezi'nde istediğiniz aramayı yapın.
+2. Sonuçların listelendiği https://tez.yok.gov.tr/UlusalTezMerkezi/tezSorguSonucYeni.jsp sayfası açıldığında F12 tuşuna basarak tarayıcınızın geliştirici araçlarını açın ve **Console** sekmesine geçin.
+3. **index.js** dosyasında bulunana kodu kopyalayın. Konsola kodu yapıştırıp **Enter** tuşuna basın.
+5. Sorgulama sonucunda dönen tüm tezlerin detaylı verilerini Excel dosyası olarak indirin.
+
+**Notlar:** 
+1. Bu kodun kullanımı, YÖK Ulusal Tez Merkezi'nin kullanım koşullarına uygun olmalıdır. Veri toplama ve kullanma konusunda YÖK'ün politikalarını göz önünde bulundurun.
+2. YÖK Ulusal Tez Merkezi'nde yapılan aramalar, tek seferde en fazla 2000 tezin verisini listelemektedir. 
+    >Tarama sonucunda 13472 kayıt bulundu. 2000 tanesi görüntülenmektedir. 
+    
+    şeklinde bir uyarıyla karşılaşmanız halinde arama kriterlerinizi arttırarak listelenen tez sayısını 2000'in altına düşürmeye çalışın. 
+    
+    Örneğin arama yaptığınız kriterleri sağlayan toplam 5000 civarı tez varsa onar yıllık periyotlar halinde sorgulama yaparsanız tek seferde listelenen tez sayısı muhtemelen 2000'in altına düşecektir.
+
+3. Bu kod 15.03.2024 tarihi itibarıyla çalışmaktadır. Ancak sayfanın kodlarında değişiklik yapılması durumunda kodun çalışmayı durdurma ihtimali her zaman mevcuttur.
---
+++ index.js
@@ -0,0 +1,118 @@
+/**
+ * Tez Sorgu Sonuç Sayfasında (https://tez.yok.gov.tr/UlusalTezMerkezi/tezSorguSonucYeni.jsp)
+ * o an listelenen tüm tezlerin ID'lerini döndürür.
+ *
+ * @returns {String[]}
+ */
+function getThesesIDs() {
+  const ids = getData().rows.map(
+    (x) => x.userId.match(/(?<=tezDetay\(')[^']*/)[0]
+  );
+
+  return ids;
+}
+
+async function getDataById(id) {
+  const url = `https://tez.yok.gov.tr/UlusalTezMerkezi/tezDetay.jsp?id=${id}`;
+  let doc;
+  return fetch(url)
+    .then((x) => x.text())
+    .then((x) => {
+      doc = new DOMParser().parseFromString(x, "text/html");
+      const thesisNo = Number(
+        doc
+          .querySelector(
+            "body > table > tbody > tr:nth-child(2) > td:nth-child(1)"
+          )
+          .innerText.trim()
+      );
+      const firstTable = [
+        ...doc.querySelector(
+          "body > table > tbody > tr:nth-child(2) > td:nth-child(3)"
+        ).childNodes,
+      ].map((x) => x.data);
+      const [thesisTitleOriginal, thesisTitleTranslation] = firstTable[0]
+        .trim()
+        .split(" /");
+      const author = firstTable[2].match(/(?<=Yazar:).+/)[0].trim();
+      const supervisor = firstTable[4].match(/(?<=Danışman:).+/)[0].trim();
+      const where = firstTable[6].match(/(?<=Yer Bilgisi:).+/)[0].trim();
+      const subject = firstTable[8].match(/(?<=Konu:).+/)[0].trim();
+      const keyword =
+        (/(?<=Dizin:).+/.test(firstTable[10]) &&
+          firstTable[10].match(/(?<=Dizin:).+/)[0].trim()) ||
+        null;
+
+      const secondTable = [
+        ...doc.querySelector(
+          "body > table > tbody > tr:nth-child(2) > td:nth-child(4)"
+        ).childNodes,
+      ].map((x) => x.data);
+      const type = secondTable[2].trim();
+      const language = secondTable[4].trim();
+      const year = Number(secondTable[6].trim());
+      const numPages = Number(secondTable[8].trim().match(/\d+/)[0]);
+
+      const abstractTurkish = doc
+        .querySelector("#td0")
+        .innerText.trim()
+        .slice(0, 5000);
+      const abstractEnglish = doc
+        .querySelector("#td1")
+        .innerText.trim()
+        .slice(0, 5000);
+
+      return {
+        "Tez No": thesisNo,
+        "Tez ID": id,
+        "Tez Adı (Orijinal)": thesisTitleOriginal,
+        "Tez Adı (Çeviri)": thesisTitleTranslation,
+        Yazar: author,
+        Danışman: supervisor,
+        Üniversite: where,
+        Konu: subject,
+        Dizin: keyword,
+        Tür: type,
+        Dil: language,
+        Yıl: year,
+        "Sayfa Sayısı": numPages,
+        "Özet (Türkçe)": abstractTurkish,
+        "Özet (İngilizce)": abstractEnglish,
+      };
+    })
+    .catch((err) => console.error(id, err));
+}
+
+function exportToExcel(data) {
+  const script = document.createElement("script");
+  script.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.13.1/xlsx.full.min.js";
+  document.body.appendChild(script);
+
+  script.onload = () => {
+    const ws = XLSX.utils.json_to_sheet(data);
+    var wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "Tez Verileri");
+    XLSX.writeFile(wb, "Tez Verileri.xlsx");
+  };
+}
+
+function main() {
+  const result = [];
+  const ids = getThesesIDs();
+  let counter = 0;
+
+  ids.forEach(async (x) => {
+    try {
+      const thesisData = await getDataById(x);
+      result.push(thesisData);
+    } 
+    catch (err) {
+      console.error(err);
+    }
+
+    if (++counter >= ids.length) exportToExcel(result);
+  });
+}
+
+main();
